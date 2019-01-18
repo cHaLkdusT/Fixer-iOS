122be3c55e83dc6635e7c44c4f1462b7187ea650
Julius Lundang <cHaLkdusT@users.noreply.github.com>
2019-01-18 22:17:29 +0800

    :sparkles: Added sample code on how to use supportedSymbols

--- Example/Fixer/SupportedSymbolsViewController.swift
+++ Example/Fixer/SupportedSymbolsViewController.swift
@@ -8,39 +8,62 @@
 
 import UIKit
 
+import Fixer
+import MBProgressHUD
+
 class SupportedSymbolsViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+  weak var fx: Fixer!
+  var symbols: [(symbol: String, description: String?)] = []
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Uncomment the following line to preserve selection between presentations
+    // self.clearsSelectionOnViewWillAppear = false
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    
+    MBProgressHUD.showAdded(to: self.view, animated: true)
+    
+    fx.supportedSymbols { response, error in
+      if let symbolsObj = response?["symbols"] as? [String: String] {
+        let keys = [String] (symbolsObj.keys)
+        var symbols = [(symbol: String, description: String?)]()
+        for key in keys.sorted() {
+          symbols.append((symbol: key, description: symbolsObj[key]))
+        }
+        self.symbols = symbols
+        
+        DispatchQueue.main.async {
+          self.tableView.reloadData()
+          MBProgressHUD.hide(for: self.view, animated: true)
+        }
+      }
     }
+  }
 
-    // MARK: - Table view data source
+  // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return symbols.count
+  }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "SupportedSymbolCell", for: indexPath)
+    
+    let (symbol, description) = symbols[indexPath.item]
+    // Configure the cell...
+    cell.textLabel?.text = description
+    cell.detailTextLabel?.text = symbol
+    
+    return cell
+  }
 
     /*
     // Override to support conditional editing of the table view.
